93f19faff6563469ae2decb37c98c8311ead5575
Viktor Walter <viktor_walter@outlook.com>
2018-04-12 13:40:48 +0200

    It now publishes.

--- src/uvled/src/uvcam.cc
+++ src/uvled/src/uvcam.cc
@@ -1,3 +1,6 @@
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <ros/ros.h>
 #include <undistortFunctions/ocam_functions.h>
 #include <functional>
 #include <gazebo/common/common.hh>
@@ -26,14 +29,17 @@ private:
   gazebo::physics::WorldPtr world;
   physics::EntityPtr        parent;
   /* bool                     ledState[20]; */
-  math::Pose        ledPose;
-  double            ledProj[2];
-  double            ledIntensity;
-  struct ocam_model oc_model;
-  cv::Mat           currImage;
-  double            coef[3] = {1.3398, 31.4704, 0.0154};
-  std::mutex        imgMtx;
-  std::thread       draw_thread;
+  math::Pose                       ledPose;
+  double                           ledProj[2];
+  double                           ledIntensity;
+  struct ocam_model                oc_model;
+  cv::Mat                          currImage;
+  double                           coef[3] = {1.3398, 31.4704, 0.0154};
+  std::mutex                       imgMtx;
+  std::thread                      draw_thread;
+  ros::NodeHandle                  nh;
+  image_transport::ImageTransport *it;
+  image_transport::Publisher       pub;
 
 public:
   void Load(sensors::SensorPtr _parent, sdf::ElementPtr _sdf) {
@@ -83,12 +89,19 @@ public:
     /*   common::Time::MSleep(1000*T); */
 
     /* } */
+    char *dummy = NULL;
+    int   zero  = 0;
+    ros::init(zero, &dummy, "uv_bluefox_emulator");
+    it = new image_transport::ImageTransport(nh);
+    char camTopicName[30];
+    sprintf(camTopicName, "%s/bluefox/image_raw", parentName.substr(0, parentName.find(":")).c_str());
+    pub = it->advertise(camTopicName, 1);
   }
 
   void Init() {
     /* for (int i    = 0; i++; i < 20) */
     /* ledState[i] = false; */
-    get_ocam_model(&oc_model, (char *)"/home/viktor/gazebo_plugins/src/uvcam/config/calib_results.txt");
+    get_ocam_model(&oc_model, (char *)"/home/viktor/gazebo_plugins/src/uvled/config/calib_results.txt");
     ResetCurrImage();
     this->updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&UvCam::OnUpdate, this));
     draw_thread            = std::thread(&UvCam::DrawThread, this);
@@ -96,14 +109,23 @@ public:
 
 private:
   void DrawThread() {
-    while (true) {
+    ros::Rate r(f);      // 10 h
+    while (ros::ok()) {
       imgMtx.lock();
       cv::GaussianBlur(currImage, currImage, cv::Size(3, 3), 0, 0);
-      cv::imshow("cv_fl_test", currImage);
+      /* cv::imshow("cv_fl_test", currImage); */
+      /* cv::waitKey((int)(T * 1000)); */
+
+      sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", currImage).toImageMsg();
+
+      /* ros::Rate loop_rate(5); */
+      /* while (nh.ok()) { */
+      /* std::cout << "publishing image" << std::endl; */
+      pub.publish(msg);
       ResetCurrImage();
       imgMtx.unlock();
-      cv::waitKey((int)(T * 1000));
-      /* cv::waitKey((int)(10)); */
+      r.sleep();
+      /* cv::waitKey((int)(T * 1000)); */
     }
   }
 
@@ -117,14 +139,13 @@ public:
     /* std::cout << "sending" << std::endl; */
     // Apply a small linear velocity to the model.
     pose = sensor->Pose() + parent->GetWorldPose().Ign();
-
   }
   // Called by the world update start event
 public:
   void poseCB(ConstPosePtr &i_pose) {
     /* math::Pose poseDiff = msgs::ConvertIgn(*i_pose) - pose; */
     /* std::cout << poseDiff.pos.x << std::endl; */
-    ledPose         = msgs::ConvertIgn(*i_pose);
+    ledPose                    = msgs::ConvertIgn(*i_pose);
     math::Quaternion invOrient = ledPose.rot;
     invOrient.Invert();
     auto   diffPose = (ledPose - pose);
@@ -162,8 +183,8 @@ public:
     /*           << "proj: [" << ledProj[1] << ":" << ledProj[0] << "]" << std::endl; */
     /* } */
     imgMtx.lock();
-    if (ledIntensity>0){
-    cv::circle(currImage, cv::Point2i(ledProj[1], ledProj[0]), ledIntensity, cv::Scalar(255), -1);
+    if (ledIntensity > 0) {
+      cv::circle(currImage, cv::Point2i(ledProj[1], ledProj[0]), ledIntensity, cv::Scalar(255), -1);
     }
     imgMtx.unlock();
   }
