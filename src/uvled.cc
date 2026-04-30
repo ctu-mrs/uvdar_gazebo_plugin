e7e5a9f3ce1c15e26112695c3d8c03386a361b39
TimLakemann <tim.lakemann@gmail.com>
2026-04-30 16:48:46 +0200

    wip cam + led

--- src/uvled.cc
+++ src/uvled.cc
@@ -72,6 +72,7 @@ private:
 
   rclcpp::Node::SharedPtr nh;
   std::string device_id;
+  int link_entity;
   std::string link_name;
   double fs = 60.0; // Default frequency
   double fm = 60.0;
@@ -109,123 +110,119 @@ public:
   // Configure: Replaces Load()
   // Called once when the plugin is loaded into the simulation
   // --------------------------------------------------------------------------
-  void Configure(const gz::sim::Entity &_entity,
-                 const std::shared_ptr<const sdf::Element> &_sdf,
-                 gz::sim::EntityComponentManager &_ecm,
-                 gz::sim::EventManager &) override
-  {
-    this->entity_id = _entity;
+void Configure(const gz::sim::Entity &_entity,
+               const std::shared_ptr<const sdf::Element> &_sdf,
+               gz::sim::EntityComponentManager &_ecm,
+               gz::sim::EventManager &) override
+{
+  this->entity_id = _entity;
 
-    // 1. Initialize ROS 2 Context
-    if (!rclcpp::ok()) {
-        rclcpp::init(0, nullptr);
+  // --- 1. Read all SDF parameters FIRST ---
+  if (_sdf->HasElement("device_id")) {
+    auto elem = _sdf->FindElement("device_id");
+    if (elem) {
+      this->device_id = elem->Get<std::string>();
     }
-    
-    // 2. Create Node
-    // Get entity name for unique node name
-    std::string entity_name = "unknown";
-    auto nameComp = _ecm.Component<gz::sim::components::Name>(_entity);
-    if (nameComp) {
-        entity_name = nameComp->Data();
-    }
-    
-    std::string node_name = "uvdar_led_" + entity_name;
-    this->nh = rclcpp::Node::make_shared(node_name);
-
-    gzdbg << "Initializing UV LED Plugin for entity: " << entity_name << std::endl;
-
-    // 3. Read SDF Parameters
-    if (_sdf->HasElement("device_id")) {
-        auto elem = _sdf->FindElement("device_id"); 
-        if (elem) {
-            this->device_id = elem->Get<std::string>();
-        }
-        gzdbg << "LED device_id set to: " << device_id << std::endl;
-    } else {
-        this->device_id = UuidGenerator::generate_v4();
-        gzdbg << "LED device_id generated: " << device_id << std::endl;
-    }
-
-    if (_sdf->HasElement("signal_id")) {
-        auto elem = _sdf->FindElement("signal_id"); 
-        if (elem) {
-            this->id = elem->Get<int>();
-        }
-        gzdbg << "LED signal ID is " << id   << std::endl;
-    } else {
-        gzdbg << "LED signal_id not set, defaulting to -1" << std::endl;
-        this->id = -1;
-    }
-
-    if (_sdf->HasElement("frequency")) {
-        auto elem = _sdf->FindElement("frequency"); 
-        if (elem) {
-            this->fs= elem->Get<double>();
-            this->fm = this->fs;
-        }
-        gzdbg << "Initial LED bitrate is " << fs << "Hz" << std::endl;
-    } else {
-        gzdbg << "Initial LED bitrate defaulting to 60Hz." << std::endl;
-        this->fs = 60.0;
-        this->fm = 60.0;
-    }
-
-
-    // 4. Determine Link Name
-    // In Gazebo Sim, the sensor is usually attached to a link. 
-    // We try to find the parent link entity.
-    auto model = gz::sim::Model(_entity);
-    if (model.Valid(_ecm)) {
-        // If the plugin is on a model, we might need to find the specific link.
-        // For now, we assume the entity itself or its parent has the name we want.
-        // If you are attaching this to a specific link in SDF, _entity IS the link.
-        auto linkNameComp = _ecm.Component<gz::sim::components::Name>(_entity);
-        if (linkNameComp) {
-            this->link_name = linkNameComp->Data();
-        }
-    } else {
-        // Fallback to entity name
-        this->link_name = entity_name;
-    }
-
-    gzdbg << "Link Name determined as: " << link_name << std::endl;
-
-    // 5. Create Publishers
-    this->led_info_pub = this->nh->create_publisher<uvdar_gazebo_plugin::msg::LedInfo>(
-        "/gazebo/ledProperties", 10);
-    
-    this->led_message_pub = this->nh->create_publisher<uvdar_gazebo_plugin::msg::LedMessage>(
-        "/gazebo/ledMessage/" + device_id, 10);
-    
-    this->led_mode_pub = this->nh->create_publisher<std_msgs::msg::Int32>(
-        "/gazebo/ledMode/" + device_id, 10);
-
-    // 6. Create Services
-    this->srv_mode_setter = this->nh->create_service<mrs_msgs::srv::SetInt>(
-        "/gazebo/ledModeSetter/" + device_id,
-        std::bind(&UvLed::callbackSetMode, this, std::placeholders::_1, std::placeholders::_2));
-
-    this->srv_freq_setter = this->nh->create_service<mrs_msgs::srv::Float64Srv>(
-        "/gazebo/ledFrequencySetter/" + device_id,
-        std::bind(&UvLed::callbackSetFrequency, this, std::placeholders::_1, std::placeholders::_2));
-
-    this->srv_active_setter = this->nh->create_service<std_srvs::srv::SetBool>(
-        "/gazebo/ledActiveSetter/" + device_id,
-        std::bind(&UvLed::callbackSetActive, this, std::placeholders::_1, std::placeholders::_2));
-
-    this->srv_msg_sender = this->nh->create_service<uvdar_core_msgs::srv::SetLedMessage>(
-        "/gazebo/ledMessageSender/" + device_id,
-        std::bind(&UvLed::callbackSendMessage, this, std::placeholders::_1, std::placeholders::_2));
-
-    this->srv_seq_setter = this->nh->create_service<mrs_msgs::srv::SetInt>(
-        "/gazebo/ledSignalSetter/" + device_id,
-        std::bind(&UvLed::callbackSetSequence, this, std::placeholders::_1, std::placeholders::_2));
-
-    gzdbg << "UV LED Plugin configured successfully." << std::endl;
-    
-    // Initial publish
-    publishData();
   }
+  if (this->device_id.empty()) {
+    this->device_id = UuidGenerator::generate_v4();
+  }
+
+ std::cerr << "I am herei 2" << this->device_id<< std::endl;
+  if (_sdf->HasElement("signal_id")) {
+    auto elem = _sdf->FindElement("signal_id");
+    if (elem) {
+      this->id = elem->Get<int>();
+    }
+  } else {
+    this->id = -1;
+  }
+
+  if (_sdf->HasElement("frequency")) {
+    auto elem = _sdf->FindElement("frequency");
+    if (elem) {
+      this->fs = elem->Get<double>();
+      this->fm = this->fs;
+    }
+  } else {
+    this->fs = 60.0;
+    this->fm = 60.0;
+  }
+
+  // Read the link name explicitly from SDF
+  if (_sdf->HasElement("link_name")) {
+    auto elem = _sdf->FindElement("link_name");
+    if (elem) {
+      this->link_name = elem->Get<std::string>();
+    }
+  }
+ std::cerr << "I am herei 3" << this->link_name<< std::endl;
+  // --- 2. Validate model and resolve the target link entity ---
+  auto model = gz::sim::Model(_entity);
+  if (!model.Valid(_ecm)) {
+    gzerr << "[UvLed] Plugin must be attached to a model!" << std::endl;
+    return;
+  }
+
+  this->link_entity = model.LinkByName(_ecm, this->link_name);
+  if (this->link_entity == gz::sim::kNullEntity) {
+    gzerr << "[UvLed] Specified link not found: " << this->link_name << std::endl;
+    return;
+  }
+  // --- 3. Create a uniquely named ROS node ---
+  if (!rclcpp::ok()) {
+    rclcpp::init(0, nullptr);
+  }
+
+  std::string entity_name = "unknown";
+  auto nameComp = _ecm.Component<gz::sim::components::Name>(_entity);
+  if (nameComp) {
+    entity_name = nameComp->Data();
+  }
+
+  // Append device_id so multiple instances on the same model are unique
+  std::string node_name = "uvdar_led_" + entity_name + "_" + this->device_id;
+  this->nh = rclcpp::Node::make_shared(node_name);
+
+  gzmsg << "[UvLed] Plugin started! model=" << entity_name
+        << " device_id=" << this->device_id
+        << " link_name=" << this->link_name << std::endl;
+
+  // --- 4. Publishers (same as before) ---
+  this->led_info_pub = this->nh->create_publisher<uvdar_gazebo_plugin::msg::LedInfo>(
+      "/gazebo/ledProperties" + device_id, 10);
+
+  this->led_message_pub = this->nh->create_publisher<uvdar_gazebo_plugin::msg::LedMessage>(
+      "/gazebo/ledMessage/" + device_id, 10);
+
+  this->led_mode_pub = this->nh->create_publisher<std_msgs::msg::Int32>(
+      "/gazebo/ledMode/" + device_id, 10);
+
+  // --- 5. Services (same as before) ---
+  this->srv_mode_setter = this->nh->create_service<mrs_msgs::srv::SetInt>(
+      "/gazebo/ledModeSetter/" + device_id,
+      std::bind(&UvLed::callbackSetMode, this, std::placeholders::_1, std::placeholders::_2));
+
+  this->srv_freq_setter = this->nh->create_service<mrs_msgs::srv::Float64Srv>(
+      "/gazebo/ledFrequencySetter/" + device_id,
+      std::bind(&UvLed::callbackSetFrequency, this, std::placeholders::_1, std::placeholders::_2));
+
+  this->srv_active_setter = this->nh->create_service<std_srvs::srv::SetBool>(
+      "/gazebo/ledActiveSetter/" + device_id,
+      std::bind(&UvLed::callbackSetActive, this, std::placeholders::_1, std::placeholders::_2));
+
+  this->srv_msg_sender = this->nh->create_service<uvdar_core_msgs::srv::SetLedMessage>(
+      "/gazebo/ledMessageSender/" + device_id,
+      std::bind(&UvLed::callbackSendMessage, this, std::placeholders::_1, std::placeholders::_2));
+
+  this->srv_seq_setter = this->nh->create_service<mrs_msgs::srv::SetInt>(
+      "/gazebo/ledSignalSetter/" + device_id,
+      std::bind(&UvLed::callbackSetSequence, this, std::placeholders::_1, std::placeholders::_2));
+
+  gzdbg << "UV LED Plugin configured successfully." << std::endl;
+
+  publishData();
+}
 
   // --------------------------------------------------------------------------
   // Update: Replaces OnUpdate()
@@ -351,4 +348,7 @@ private:
   }
 };
 }
-GZ_ADD_PLUGIN(uvdar_gazebo_plugin::UvLed, gz::sim::System)
+GZ_ADD_PLUGIN(uvdar_gazebo_plugin::UvLed, 
+        gz::sim::System,
+    gz::sim::ISystemConfigure,
+    gz::sim::ISystemUpdate)
